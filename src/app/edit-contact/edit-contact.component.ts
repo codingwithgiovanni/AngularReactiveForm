4d6fa9585970a0932eaa881c45aa47afda797836
Giovanni Crudele <giovannic@msys.it>
2024-01-22 15:49:59 +0100

    First Commit

---
+++ src/app/edit-contact/edit-contact.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
+
+@Component({
+  templateUrl: './edit-contact.component.html',
+  styleUrls: ['./edit-contact.component.css']
+})
+export class EditContactComponent implements OnInit {
+  contactForm = new FormGroup({
+    firstName :  new FormControl,
+    lastName: new FormControl,
+    dateOfBirth: new FormControl,
+    favoritesRanking: new FormControl
+  })
+
+
+  constructor(private route: ActivatedRoute,private contactsService:ContactsService) { }
+
+  ngOnInit() {
+    const contactId = this.route.snapshot.params['id'];
+    if (!contactId) return
+
+
+    this.contactsService.getContact(contactId).subscribe((contact)=> {
+      if(!contact) null;
+
+      this.contactForm.controls.firstName.setValue(contact?.firstName);
+      this.contactForm.controls.lastName.setValue(contact?.lastName);
+      this.contactForm.controls.dateOfBirth.setValue(contact?.dateOfBirth);
+      this.contactForm.controls.favoritesRanking.setValue(contact?.favoritesRanking);
+    })
+  }
+
+  saveContact() {
+    console.log(this.contactForm.value);
+    // console.log(this.contactForm.controls.firstName.value)
+    // console.log(this.contactForm.controls.lastName.valid)
+    // console.log(this.contactForm.controls.dateOfBirth.value)
+    // console.log(this.contactForm.controls.favoritesRanking.value)
+
+
+  }
+}
